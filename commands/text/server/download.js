b605afbb476c7ca5d7007d7aaa31fc461a5aa7bc
iTsMaaT <matoulimar1@gmail.com>
2024-02-17 21:36:17 -0500

    download and dashboard

---
+++ commands/text/server/download.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+const { addToQueue } = require("../../../utils/QueueSystem.js");
+
+module.exports = {
+    name: "downloads",
+    description: "Downloads a youtube video / playlist for Jellyfin",
+    category: "server",
+    aliases: ["dl"],
+    async execute(logger, client, message, args) {
+        const videoUrl = args[1];
+        const outputFolder = "/mnt/Main/Jellyfin/YT-DLP";
+        const downloadCommand = `yt-dlp -f 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[height<=1080][ext=mp4]/best' -o '${outputFolder}/%(title)s.%(ext)s' ${videoUrl}`;
+        const downloadOperation = () => new Promise((resolve, reject) => {
+            // Execute yt-dlp command to download the video as MP4 and in 1080p resolution
+            exec(downloadCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    reject("An error occurred while downloading the video.");
+                }
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    reject("An error occurred while downloading the video.");
+                }
+                console.log(`stdout: ${stdout}`);
+                resolve("Video downloaded successfully!");
+            });
+        });
+    
+        // Add the download operation to the queue
+        addToQueue("download", downloadOperation)
+            .then(result => message.reply(result))
+            .catch(error => message.reply(error));
+    },
+};
