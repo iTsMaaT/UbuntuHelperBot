779cc06c60a17ccb8ea0f54b2306b3a588286aa4
iTsMaaT <matoulimar1@gmail.com>
2024-02-22 18:25:40 -0500

    a

--- commands/text/server/downloadmusic.js
+++ commands/text/server/downloadmusic.js
@@ -36,17 +36,21 @@ module.exports = {
                 await promptMessage.delete();
                 await collected.first().delete();
 
-                if (promptObj.name === "cover" && content.toLowerCase() === "skip") {
-                    metadata.cover = null;
+                if (promptObj.name === "cover") {
+                    if (promptObj.content == null) {
+                        metadata.cover = null;
+                    } else {
+                        try {
+                            const response = await (await fetch(content)).buffer();
+                            const coverBuffer = await response.buffer();
+                            metadata.cover = coverBuffer;
+                        } catch (error) {
+                            return message.reply(`Error fetching cover image: ${error.message}`);
+                        }
+                    }
+                    
                 } else {
                     metadata[promptObj.name] = content;
-                    try {
-                        const response = await (await fetch(content)).buffer();
-                        const coverBuffer = await response.buffer();
-                        metadata.cover = coverBuffer;
-                    } catch (error) {
-                        return message.reply(`Error fetching cover image: ${error.message}`);
-                    }
                 }
                 
             }
